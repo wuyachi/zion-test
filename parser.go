81f04731b5298734a550b48bd5c55b6995de2028
wuyachi <wuyachi@163.com>
2022-08-22 11:12:04 +0800

    Add withdrawValidator parse handler

--- parser.go
+++ parser.go
@@ -35,6 +35,8 @@ func NewParseHandler(rawAction *RawAction) (ParseHandler, error) {
 	switch rawAction.MethodName {
 	case base.MethodCreateValidator:
 		return &CreateValidatorParser{rawAction: rawAction}, nil
+	case base.MethodWithdrawValidator:
+		return &WithdrawValidatorParser{rawAction: rawAction}, nil
 	case base.MethodUpdateValidator:
 		return &UpdateValidatorParser{rawAction: rawAction}, nil
 	case base.MethodCancelValidator:
