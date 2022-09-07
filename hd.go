d67daa854de1fd36c67f17d80b53e1f11f7945f5
wuyachi <wuyachi@163.com>
2022-09-07 15:17:43 +0800

    Update user initial balance

--- hd.go
+++ hd.go
@@ -85,14 +85,18 @@ func dump(ctx *cli.Context) (err error) {
 	var validators []map[string]string
 	var seeds, addresses []string
 	unit := new(big.Int).Exp(big.NewInt(10), big.NewInt(18), nil)
-	allocPerUser := new(big.Int).Mul(big.NewInt(1000000), unit)
+	allocPerUser := new(big.Int).Mul(big.NewInt(100000), unit)
 	allocPerStaker := new(big.Int).Mul(big.NewInt(2000000), unit)
-	left := new(big.Int).Mul(big.NewInt(100000000-1000000*50-2000000*12), unit)
+	left := new(big.Int).Mul(big.NewInt(100000000-100000*99-2000000*13), unit)
 
 	var privs, pubs []string
 
-	for i := 0; i < 50; i++ {
-		w := &HDAddress{0, uint32(i)}
+	for i := 0; i < 100; i++ {
+		w := &HDAddress{0, uint32(i + 1)}
+		if i == 28 {
+			alloc[w.ToAddress().Hex()] = map[string]string{"balance": allocPerStaker.String()}
+			continue
+		}
 		alloc[w.ToAddress().Hex()] = map[string]string{"balance": allocPerUser.String()}
 	}
 	for i := 0; i < 12; i++ {
